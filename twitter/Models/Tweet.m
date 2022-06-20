279cdca3d7aa3a13b8f37d25a3b2ca6ecf3a56a2
Catherine Lu <catherinelu@fb.com>
2022-06-20 13:55:41 -0700

    Implement User and Tweet data models

---
+++ twitter/Models/Tweet.m
@@ -0,0 +1,61 @@
+//
+//  Tweet.m
+//  twitter
+//
+//  Created by Catherine Lu on 6/20/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import "Tweet.h"
+#import "User.h"
+
+@implementation Tweet
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+
+    if (self) {
+        NSDictionary *originalTweet = dictionary[@"retweeted_status"];
+        if (originalTweet != nil) {
+            NSDictionary *userDictionary = dictionary[@"user"];
+            self.retweetedByUser = [[User alloc] initWithDictionary:userDictionary];
+
+            dictionary = originalTweet;
+        }
+        self.idStr = dictionary[@"id_str"];
+        self.text = dictionary[@"text"];
+        self.favoriteCount = [dictionary[@"favorite_count"] intValue];
+        self.favorited = [dictionary[@"favorited"] boolValue];
+        self.retweetCount = [dictionary[@"retweet_count"] intValue];
+        self.retweeted = [dictionary[@"retweeted"] boolValue];
+
+        // initialize user
+        NSDictionary *user = dictionary[@"user"];
+        self.user = [[User alloc] initWithDictionary:user];
+
+        // format createdAt date string
+        NSString *createdAtOriginalString = dictionary[@"created_at"];
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
+        
+        // convert String to Date
+        NSDate *date = [formatter dateFromString:createdAtOriginalString];
+        formatter.dateStyle = NSDateFormatterShortStyle;
+        formatter.timeStyle = NSDateFormatterNoStyle;
+        
+        // convert Date to String
+        self.createdAtString = [formatter stringFromDate:date];
+    }
+    return self;
+}
+
++ (NSMutableArray *)tweetsWithArray:(NSArray *)dictionaries {
+    NSMutableArray *tweets = [NSMutableArray array];
+    for (NSDictionary *dictionary in dictionaries) {
+        Tweet *tweet = [[Tweet alloc] initWithDictionary:dictionary];
+        [tweets addObject:tweet];
+    }
+    return tweets;
+}
+
+@end

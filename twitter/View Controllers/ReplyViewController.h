27d6133f93cbda4269bc5a44908bb9eb0b0fc945
Catherine Lu <catherinelu@fb.com>
2022-06-23 11:16:08 -0700

    Added reply functionality from timeline view

---
+++ twitter/View Controllers/ReplyViewController.h
@@ -0,0 +1,17 @@
+//
+//  ReplyViewController.h
+//  twitter
+//
+//  Created by Catherine Lu on 6/23/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ReplyViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/Views/ReplyButton.h
@@ -0,0 +1,17 @@
+//
+//  ReplyButton.h
+//  twitter
+//
+//  Created by Catherine Lu on 6/23/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ReplyButton : UIButton
+
+@end
+
+NS_ASSUME_NONNULL_END
